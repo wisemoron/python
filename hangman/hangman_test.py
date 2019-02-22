78ae183916b274d205b1850cee5221125949bb98
wisemoron <47875402+wisemoron@users.noreply.github.com>
2019-02-22 08:45:57 -0500

    find letter

--- hangman/hangman_test.py
+++ hangman/hangman_test.py
@@ -1,3 +1,39 @@
-print("hello")
-print("world")
-print("better now lets see")
+import sys
+letter = []
+numtries = 10
+letters_found = 0
+wordlist = ["starscream", "megatron"]
+
+def lay_board(wordlist):
+    for dash in range(len(wordlist[0])):
+        sys.stdout.write ('__ ')
+    return
+
+def accept_input(letter):
+    print("")
+    ip_letter = input()
+    if ip_letter not in letter:
+        letter.append(ip_letter)
+    return
+
+def find_letter_in_word(wordlist, letter, letters_found):
+    for pos, char in enumerate(wordlist[0]):
+        if char in letter:
+            sys.stdout.write(char.upper() + ' ')
+            letters_found = letters_found + 1
+        else:
+            sys.stdout.write('__ ')
+    return letters_found
+
+#lay_board(wordlist)
+
+#while letters_found != 10:
+letters_found = find_letter_in_word(wordlist, letter, letters_found)
+print(letters_found)
+accept_input(letter)
+letters_found = find_letter_in_word(wordlist, letter, letters_found)
+print(letters_found)
+accept_input(letter)
+letters_found = find_letter_in_word(wordlist, letter, letters_found)
+print(letters_found)
+
